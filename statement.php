67162cae609bb889fe462fa1e05aa8b13cb5d552
GoranACM <goranacm@gmail.com>
2018-10-11 13:37:06 +1100

    Add files via upload

--- statement.php
+++ statement.php
@@ -1,11 +1,6 @@
 <?php
 
-$servername = "localhost";
-// $username = "id6957991_g0r4nm1k3rud0lf0";
-// $password = "!mm3r53YW";
-$username = "root";
-$password = "";
-$dbname = "id6957991_immerseyourworld";
+include 'serverCreds.php';
 
 //create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
